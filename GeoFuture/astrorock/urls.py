60785fb988dfd3c1ffd84837fc1a54bddc17fadc
eddygulled <edgargulay69@gmail.com>
2023-10-08 00:37:40 +0300

    mapbox pause

--- GeoFuture/astrorock/urls.py
+++ GeoFuture/astrorock/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.init_view, name='init_view')
+    path('', views.init_view, name='init_view'),
+    path('interactive-map/', views.interactive_maps_view, name='interactive_maps_view'),
 ]
--- GeoFuture/astrorock/views.py
+++ GeoFuture/astrorock/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def init_view(request):
     return render(request, "index.html")
+
+
+def interactive_maps_view(request):
+    return render(request, "pages/interactive_map.html")

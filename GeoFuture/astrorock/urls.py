0ca4ba67e1f524bb6fc2d0be55d98349ddee4f4a
eddygulled <edgargulay69@gmail.com>
2023-10-08 04:22:07 +0300

    template pause

--- GeoFuture/astrorock/urls.py
+++ GeoFuture/astrorock/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.init_view, name='init_view'),
     path('interactive-map/', views.interactive_maps_view, name='interactive_maps_view'),
+    path('geological-analysis/', views.analysis_view, name='analysis_view'),
+    path('data-visualization/', views.visualization_view, name='visualization_view')
 ]
--- GeoFuture/astrorock/views.py
+++ GeoFuture/astrorock/views.py
@@ -7,3 +7,9 @@ def init_view(request):
 
 def interactive_maps_view(request):
     return render(request, "pages/interactive_map.html")
+
+def analysis_view(request):
+    return render(request, "pages/analytic_tools.html")
+
+def visualization_view(request):
+    return render(request, "pages/data_visualization.html")
